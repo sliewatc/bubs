556f92a2c4a3bc80917144c47062ae6664814fc4
Sean Liew <seanliew@seans-mbp.mynetworksettings.com>
2024-11-26 21:04:48 -0500

    second test

---
+++ bub-bday/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    let { children } = $props();
+</script>
+
+<master>
+    {@render children()}
+</master>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: rgba(255, 167, 224, 1);
+        width: 100vw;
+        display: block;
+    }
+</style>
